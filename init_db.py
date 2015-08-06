fcbfe050d47de60fcfee3ebcc4d25f3a62f2b5aa
akliang <none@none.com>
2015-08-06 17:17:43 -0400

    fixed bug in init_db script

--- init_db.py
+++ init_db.py
@@ -17,6 +17,7 @@ def start(dbpath):
   
   c.execute('create table user_info(id integer primary key, email text, name text, password text);')
   c.execute('create table purchase_history(id integer primary key, emailid integer, product_asin text, product_name text, purchase_date integer, starting_price real);')
+  c.execute('create table purchase_cache as select * from purchase_history where 0;')
   c.execute('create table product_history(id integer primary key, product_asin text, product_name text, date integer, price real, medium_image_url text, offer_url text);')
   c.execute('create table product_cache as select * from product_history where 0;')
   
@@ -26,7 +27,7 @@ def start(dbpath):
   c.execute("INSERT INTO purchase_history VALUES ('1', 'swagberry@meowbucks.com', 'B00QJDU3KY', 'Kindle', '1438894338', '100')")
   c.execute("INSERT INTO purchase_history VALUES ('2', 'swagberry@meowbucks.com', 'B00JP7R8X6', 'Kindle Cover', '1438894038', '10')")
   c.execute("INSERT INTO purchase_history VALUES ('3', 'jojo@meowbucks.com', 'B00JP7R8X6', 'Kindle Cover', '1438892338', '10')")
-  c.execute('create table purchase_cache as select * from purchase_history where 0;')
+  c.execute("INSERT INTO purchase_cache SELECT * FROM purchase_history")
   
   conn.commit()
   conn.close()
