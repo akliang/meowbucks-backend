786e1d4a003b6f66c66c0bfe2a1c16c3413d8895
Albert Liang <albert.liang@gmail.com>
2015-08-11 22:18:48 -0400

    work in progress of processURL, not complete

--- processURL.py
+++ processURL.py
@@ -8,63 +8,15 @@ import logging
 import os
 import init_db
 import re
+import mbcommon as mbc
 
-if os.path.exists('/Users/Sherry/meowbucks/'):
-    homedir = '/Users/Sherry/meowbucks'
-else:
-    homedir='.'
-
+homedir = mbc.set_working_path()
 dbpath = "%s/mb_databases/mb_test_db.db" % (homedir)
 logpath = "%s/log_files/apicall_log_%s.log" % (homedir,datetime.now().strftime("%Y_%m_%d_%H_%M"))
-
-# print('Homedir: '+homedir)
-# print('Logpath: '+logpath)
-# print('DBpath:  '+dbpath)
 
 
 # Create daily log file
 logging.basicConfig(filename=logpath,level=logging.DEBUG)
-
-
-# Get a list of asins to send to api
-def get_asin_list(c):
-    asin_list = []
-
-    for row in c.execute('SELECT DISTINCT product_asin FROM purchase_cache'):
-        row = row[0].encode('utf-8')
-        asin_list.append(row)
-
-    return asin_list
-
-
-# Call amazon api
-def call_API(asin):
-    AMAZON_ACCESS_KEY = 'AKIAJ2MUVQ36XFWLYUEA'
-    AMAZON_SECRET_KEY = '***REMOVED***z'
-    AMAZON_ASSOC_TAG = 'meowbucks-20'
-
-    amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
-    product = amazon.lookup(ItemId = asin)
-
-    if product:
-        # fields returned from Amazon
-        price = product.price_and_currency[0]
-        title = product.title
-        call_date = date.today()
-        img = product.medium_image_url
-        url = product.offer_url
-
-        return price, asin, title, call_date, img, url
-
-
-def insert_into_table(c,table_name,resp):
-    if table_name in ('product_history','product_cache'):
-        price, asin, title, call_date, img, url = resp
-        query = """INSERT INTO product_cache
-                (product_asin, product_name, date, price, medium_image_url, offer_url) 
-                VALUES ('%s','%s','%s','%s','%s','%s')""" %(asin,title,call_date,price,img,url)
-        c.execute(query)
-    return
 
 
 def main():
@@ -99,7 +51,7 @@ def main():
     # insert the data into the db
     mb_database = sqlite3.connect(dbpath)
     c = mb_database.cursor()
-    insert_into_table(c,'purchase_history',
+    %insert_into_table(c,'purchase_history',
     
 
 
