a0ca941b1c3308346f6295dfe728359e5f6c9553
akliang <none@none.com>
2015-08-13 23:12:19 -0400

    cleaned up amazonAPI to use the new mbcommon library

--- amazonAPI.py
+++ amazonAPI.py
@@ -4,24 +4,15 @@
     3. populate the product_info page with data returned from the API
 '''
 
-from amazon.api import AmazonAPI
 import sqlite3
 from datetime import date, datetime
 import logging
-import os
 import mbcommon as mbc
 
-if os.path.exists('/Users/Sherry/meowbucks/'):
-    homedir = '/Users/Sherry/meowbucks'
-else:
-    homedir='.'
 
+homedir = mbc.set_working_path()
 dbpath = "%s/mb_databases/mb_test_db.db" % (homedir)
 logpath = "%s/log_files/apicall_log_%s.log" % (homedir,datetime.now().strftime("%Y_%m_%d_%H_%M"))
-
-# print('Homedir: '+homedir)
-# print('Logpath: '+logpath)
-# print('DBpath:  '+dbpath)
 
 
 # Create daily log file
@@ -39,42 +30,6 @@ def get_asin_list(c):
     return asin_list
 
 
-# Call amazon api
-def call_API(asin):
-    AMAZON_ACCESS_KEY = 'AKIAJ2MUVQ36XFWLYUEA'
-    AMAZON_SECRET_KEY = '***REMOVED***z'
-    AMAZON_ASSOC_TAG = 'meowbucks-20'
-
-    amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
-    product = amazon.lookup(ItemId = asin)
-
-    if product:
-        # fields returned from Amazon
-        price = product.price_and_currency[0]
-        title = product.title
-        call_date = date.today()
-        img = product.medium_image_url
-        url = product.offer_url
-
-        return price, asin, title, call_date, img, url
-
-
-## Updated table using data returned from api
-#def update_product_table(c, asin, title, call_date, price, img, url):
-#    query = """INSERT INTO product_cache
-#            (product_asin, product_name, date, price, medium_image_url, offer_url) 
-#            VALUES ('%s','%s','%s','%s','%s','%s')""" %(asin,title,call_date,price,img,url)
-#    c.execute(query)
-
-
-def insert_into_table(c,table_name,resp):
-    if table_name in ('product_history','product_cache'):
-        price, asin, title, call_date, img, url = resp
-        query = """INSERT INTO product_cache
-                (product_asin, product_name, date, price, medium_image_url, offer_url) 
-                VALUES ('%s','%s','%s','%s','%s','%s')""" %(asin,title,call_date,price,img,url)
-        c.execute(query)
-    return
 
 
 def main():
@@ -92,12 +47,10 @@ def main():
     if len(asin_list) > 0:
         for asin in asin_list:
 
-            resp = call_API(asin)
+            resp = mbc.call_API(asin)
             if resp:
-                #price, asin, title, call_date, img, url = resp
-                #update_product_table(c, asin, title, call_date, price, img, url)
-                insert_into_table(c,'product_history',resp)
-                insert_into_table(c,'product_cache',resp)
+                mbc.insert_into_table(c,'product_history',resp)
+                mbc.insert_into_table(c,'product_cache',resp)
                 successfully_updated += 1
             else:
                 logging.warning("Amazon did not find asin: %s" %(asin))
