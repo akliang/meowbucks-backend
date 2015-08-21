d1f5c3fc4f550aa77f8ab55e39539e60975eac01
akliang <none@none.com>
2015-08-20 22:25:36 -0400

    fixed sendmail to multiple recipients and changed from email address

--- send_email.py
+++ send_email.py
@@ -15,16 +15,18 @@ def send_mail(SUBJECT, BODY, TO, FROM):
     HTML_BODY = MIMEText(BODY, 'html')
     MESSAGE.attach(HTML_BODY)
  
-    server = smtplib.SMTP('smtp.gmail.com:587')
+    #server = smtplib.SMTP('smtp.gmail.com:587')
+    server = smtplib.SMTP('mail.hover.com:587')
  
     if __name__ == "__main__":
         server.set_debuglevel(1)
  
-    password = "mbpowercat"
+    #password = "mbpowercat"
+    password = "Elsasaysmeow"
  
     server.starttls()
     server.login(FROM,password)
-    server.sendmail(FROM, [TO], MESSAGE.as_string())
+    server.sendmail(FROM, TO2, MESSAGE.as_string())
     server.quit()
  
 if __name__ == "__main__": 
@@ -134,6 +136,7 @@ if __name__ == "__main__":
     #TO = 'sherry415@gmail.com; albert.liang@gmail.com; kevin.cheng76@gmail.com'
     TO = "sonic_down@yahoo.com ; albert.liang@gmail.com, sherry415@gmail.com, kevin.cheng76@gmail.com"
     TO2 = ['sonic_down@yahoo.com', 'albert.liang@gmail.com','sherry415@gmail.com','kevin.cheng76@gmail.com']
-    FROM ='sherry.meowbucks@gmail.com'
+    #FROM ='sherry.meowbucks@gmail.com'
+    FROM ='hello@meowbucks.com'
  
     send_mail("Your Amazon Item Reduced in Price!", email_content, TO, FROM)
