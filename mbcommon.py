f183501343717f86698c4ff8567caeeb214b5ef3
akliang <none@none.com>
2015-08-21 15:25:35 -0400

    added sendmail function to mbcommon, also changed db structure a little bit, and also if you run mbcommon as a script, it will regenerate the db

--- mbcommon.py
+++ mbcommon.py
@@ -3,6 +3,9 @@ import sqlite3
 import os
 from amazon.api import AmazonAPI
 from datetime import date, datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 class amazonItem:
@@ -14,14 +17,14 @@ def reset_db(dbpath):
     c = conn.cursor()
     
     # Uncomment below section if want to create tables purchases_tracking and product_info from scratch
-    c.execute('DROP TABLE IF EXISTS user_info')
+    c.execute('DROP TABLE IF EXISTS user_login')
     c.execute('DROP TABLE IF EXISTS purchase_history')
     c.execute('DROP TABLE IF EXISTS purchase_cache')
     c.execute('DROP TABLE IF EXISTS product_history')
     c.execute('DROP TABLE IF EXISTS product_cache')
     
     
-    c.execute('create table user_info(id integer primary key, email text, name text, password text);')
+    c.execute('create table user_login(id integer primary key, email text, token text, ttl integer);')
     c.execute('create table purchase_history(id integer primary key, emailid integer, product_asin text, purchase_date integer, starting_price real);')
     c.execute('create table purchase_cache as select * from purchase_history where 0;')
     c.execute('create table product_history(id integer primary key, product_asin text, date integer, price real, medium_image_url text, offer_url text);')
@@ -29,7 +32,6 @@ def reset_db(dbpath):
     
     
     
-    c.execute('INSERT INTO user_info VALUES(1,"asdf@asdf.com","asdf","asdf")')
     c.execute("INSERT INTO purchase_history VALUES ('1', 'swagberry@meowbucks.com', 'B00QJDU3KY', '1438794338', '100')")
     c.execute("INSERT INTO purchase_history VALUES ('2', 'swagberry@meowbucks.com', 'B00JP7R8X6', '1438894038', '10')")
     c.execute("INSERT INTO purchase_history VALUES ('3', 'jojo@meowbucks.com', 'B00JP7R8X6', '1438892338', '10')")
@@ -87,4 +89,34 @@ def insert_into_table(c,table_name,resp):
     return
 
 
+def send_mail(SUBJECT, BODY, TO, FROM):
+    """Send out html email"""
+
+    MESSAGE = MIMEMultipart('alternative')
+    MESSAGE['subject'] = SUBJECT
+    MESSAGE['To'] = TO
+    MESSAGE['From'] = FROM
+    MESSAGE.preamble = """
+    Your mail reader does not support the report format.
+    Please visit us <a href="http://www.meowbucks.com">online</a>!"""
+    HTML_BODY = MIMEText(BODY, 'html')
+    MESSAGE.attach(HTML_BODY)
+
+    server = smtplib.SMTP('mail.hover.com:587')
+
+    if __name__ == "__main__":
+        server.set_debuglevel(1)
+
+    password = "Elsasaysmeow"
+
+    server.starttls()
+    server.login(FROM,password)
+    server.sendmail(FROM, TO.split(','), MESSAGE.as_string())
+    server.quit()
+
+
+
+if __name__ == '__main__':
+    print "Resetting ./mb_databases/mb_test_db.db"
+    reset_db('./mb_databases/mb_test_db.db')
 
